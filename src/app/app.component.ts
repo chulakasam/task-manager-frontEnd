79f308feca13200990e554840c94be12898cbadd
chulakasam <sampathchulaka33872@gmail.com>
2025-04-07 20:19:06 +0530

    initialize project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,18 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+
+import {RouterOutlet} from '@angular/router';
+import {TaskFormComponent} from './components/task-form.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  imports: [
+
+    RouterOutlet,
+    TaskFormComponent
+  ],
+
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'task-manager';
